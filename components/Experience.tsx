1c6e8af157b0616c0bec0b62545e4a434dc6e35a
BhautikLukhi <bhautikkumar.lukhi@uni-bielefeld.de>
2025-02-26 10:04:12 +0100

    added some hrefs and one project

--- components/Experience.tsx
+++ components/Experience.tsx
@@ -1,34 +1,36 @@
 'use client';
 
-import React from 'react'
-import SectionHeading from './SectionHeading'
-import { VerticalTimeline, VerticalTimelineElement } from 'react-vertical-timeline-component'
-import 'react-vertical-timeline-component/style.min.css'
-import { experiencesData } from '@/lib/data'
+import React from 'react';
+import SectionHeading from './SectionHeading';
+import { VerticalTimeline, VerticalTimelineElement } from 'react-vertical-timeline-component';
+import 'react-vertical-timeline-component/style.min.css';
+import { experiencesData } from '@/lib/data';
 import { useSectionInView } from '@/lib/hooks';
 import { useTheme } from '@/context/theme-context';
 
 const Experience = () => {
-  const { ref } = useSectionInView("Experience", 0.5)
-  const { theme } = useTheme()
+  const { ref } = useSectionInView("Experience", 0.5);
+  const { theme } = useTheme();
+
   return (
     <section
       ref={ref}
       id="experience"
       className='scroll-mt-28 mb-28 sm:mb-40'
     >
-      <SectionHeading> My experience</SectionHeading>
+      <SectionHeading> My Experience </SectionHeading>
       <VerticalTimeline lineColor=''>
         {experiencesData.map((item, index) => (
           <React.Fragment key={index}>
             <VerticalTimelineElement
               visible={true}
               contentStyle={{
-                backgroundColor: theme === "light" ?"#f3f4f6" : "rgba(255, 255, 255, 0.05)",
+                backgroundColor: theme === "light" ? "#f3f4f6" : "rgba(255, 255, 255, 0.05)",
                 boxShadow: "none",
                 border: "1px solid rgba(0, 0, 0, 0.05)",
                 textAlign: "left",
-                padding: "1.3rem 2rem"
+                padding: "1.3rem 2rem",
+                cursor: "pointer",
               }}
               contentArrowStyle={{
                 borderRight: theme === "light" ? "0.4rem solid #9ca3af" : "0.4rem solid rgba(255, 255, 255, 0.5)",
@@ -40,15 +42,22 @@ const Experience = () => {
                 fontSize: "1.5rem"
               }}
             >
-              <h3 className='font-semibold capitalize'>{item.title}</h3>
-              <p className='font-normal !mt-0 '>{item.location}</p>
-              <p className='!mt-1 !font-normal text-gray-700 dark:text-white/75'>{item.description}</p>
+              <a 
+                href={item.url} 
+                target="_blank" 
+                rel="noopener noreferrer"
+                className="block text-black dark:text-white no-underline hover:underline"
+              >
+                <h3 className='font-semibold capitalize'>{item.title}</h3>
+                <p className='font-normal'>{item.location}</p>
+                <p className='!mt-1 !font-normal text-gray-700 dark:text-white/75'>{item.description}</p>
+              </a>
             </VerticalTimelineElement>
           </React.Fragment>
         ))}
       </VerticalTimeline>
     </section>
-  )
+  );
 }
 
-export default Experience
+export default Experience;
--- lib/data.ts
+++ lib/data.ts
@@ -1,6 +1,7 @@
 import React from "react";
 import { CgWorkAlt } from "react-icons/cg";
 import fcSchalkeImg from "@/public/fc_schalke.png";
+import squatsImg from "@/public/Squats.png";
 import hrAnalyticsImg from "@/public/hr_analytics.png";
 import salaryPredImg from "@/public/salary_pred.png";
 
@@ -33,30 +34,35 @@ export const links = [
 
 export const experiencesData = [
   {
-    title: "Bielefeld University(Department of Sports Science)",
+    title: "Student Research Assistant - Department of Sports Science",
     location: "Bielefeld, Germany",
     description:
       "Currently I am working  with Prof. Dr. Christian Deutscher on diverse sports research projects. Skilled in data scraping, statistical modeling, and visualizing insights from large datasets, including >3M data points from Bundesliga, Marathons, Hockey, and NBA L2M reports.",
     icon: React.createElement(CgWorkAlt),
     date: "May 2024 - Present",
+    url: "https://www.uni-bielefeld.de/fakultaeten/sportwissenschaft/arbeitsbereiche/ab-v/team/deutscher/",
   },
   {
-    title: "Hochschule Bielefeld",
+    title: "Research Assistant - Hochschule Bielefeld",
     location: "Bielefeld, Germany",
     description:
       "Learned and contributed to the development privacy-preserving machine learning solutions for the CareTech OWL project, leveraging Fully Homomorphic Encryption (FHE) and Concrete-ML to further securely train models on real-world sensor data while ensuring data confidentiality.",
     icon: React.createElement(CgWorkAlt),
     date: "May 2024 - Aug 2024",
+    url: "https://www.caretech-owl.de/projekte/projekt-transcaretech",
   },
   {
-    title: "Bielefeld University(Department of Mathematics)",
+    title: "Teaching Assistant - Department of Mathematics",
     location: "Bielefeld, Germany",
     description:
       "Teaching Assistant for Mathematics 1 at Bielefeld University, conducting weekly 3 in-person tutorials and collaborating with Prof. Dr. Michael Hinz and fellow tutors.",
     icon: React.createElement(CgWorkAlt),
     date: "Oct 2023 - Mar 2024",
+    url: "https://ekvv.uni-bielefeld.de/kvv_publ/publ/Lehrende_Veranstaltungen.jsp;jsessionid=6A7D0C7FA93D061C467E19DEBE593D33?personId=39878381&zeigeAlle=1"
   }
 ] as const;
+
+
 
 export const projectsData = [
   {
@@ -66,6 +72,14 @@ export const projectsData = [
     tags: ["python", "Streamlit", "Scikit-learn"],
     imageUrl: salaryPredImg,
     url: "https://salary-predictor-2024.streamlit.app",
+  },
+  {
+    title: "SquatFormGuru",
+    description:
+      "Leveraged computer vision and pose estimation to analyze squat form, track reps, angles, and posture, providing real-time feedback for injury prevention.",
+    tags: ["Python", "Open-CV", "MediaPipe", "Computer Vision", "Node.js"],
+    imageUrl: squatsImg,
+    url: "https://github.com/BhautikLukhi/SquatFormGuru",
   },
   {
     title: "FC Schalke Statistical Consulting",
@@ -105,5 +119,7 @@ export const skillsData = [
   "Excel",
   "REST-APIs",
   "MS Office Suite",
+  "React.js",
+  "Node.js",
   "German(A2-B1)",
 ] as const;
